744d7861759f6ffe1bc5f1437a3d25be5f9d6151
gaieges <evin.callahan@gmail.com>
2015-05-09 21:48:37 -0400

    basic app

--- server-app/api/app.js
+++ server-app/api/app.js
@@ -1,1 +1,28 @@
-#starting this file
+//starting this file
+var port = process.env.PORT || 8000;
+
+
+var express = require('express');
+var app     = express();
+
+console.log("Starting up web server on port " + port + " ..");
+
+
+app.get('/', function(req, res) {
+  res.type('text/plain');
+  var output = 'endpoints: \n /friends - gets all friends\n';
+  res.send( output );
+})
+
+app.get('/friends', function(req, res) {
+  res.type('text/json');                  // set content-type
+  res.send('friends show up here w/ location\n'); // send text response
+});
+
+app.get('/friends/:id', function(req, res) {
+  res.type('text/json');                  // set content-type
+  res.send("you sent " + req.params.id + "\n"); // send text response
+});
+
+
+app.listen( port );
